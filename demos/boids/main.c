9b30aacdabccbff6baaeb0690c0cee4e4e18cf36
Evgeny Iakovlev <eyakovlev3@gmail.com>
2025-06-15 01:31:32 +0200

    boids: weighted steering forces

--- demos/boids/main.c
+++ demos/boids/main.c
@@ -23,20 +23,23 @@
 #endif
 #define FLT_EPSILON 0.001
 
+#ifndef MIN
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#endif
+
+#ifndef MAX
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#endif
+
 // Simulation freq
 #define VT_HZ 60
 
 // Boid dimensions in dots.
-#define VT_BOID_WIDTH   6
+#define VT_BOID_WIDTH   7
 #define VT_BOID_LENGTH  9
 
 // Boid linear speed in dots per second.
-#define VT_BOID_SPEED        50
-
-// Boid roll angle for banking, in degrees.
-// Larger angles produce sharper turns, but should be kept under 90.
-#define VT_BOID_BANK_ANGLE   80
-_Static_assert(VT_BOID_BANK_ANGLE < 90);
+#define VT_BOID_SPEED        60
 
 // Wandering configuration.
 #define VT_BOID_AVG_HEADING_DELAY_MS        2000
@@ -46,14 +49,20 @@ _Static_assert(VT_BOID_BANK_ANGLE < 90);
 // Sensing ranges.
 #define VT_BOID_VIEW_RANGE                  80
 #define VT_BOID_VIEW_RANGE_SQUARED          (VT_BOID_VIEW_RANGE * VT_BOID_VIEW_RANGE)
-#define VT_BOID_REPULSION_RANGE             20
+#define VT_BOID_REPULSION_RANGE             15
 #define VT_BOID_REPULSION_RANGE_SQUARED     (VT_BOID_REPULSION_RANGE * VT_BOID_REPULSION_RANGE)
 _Static_assert(VT_BOID_REPULSION_RANGE < VT_BOID_VIEW_RANGE);
 
-#define VT_BOID_TRAIL_SIZE 16
+// Steering forces weights.
+#define VT_BOID_ALIGNMENT_WEIGHT            1.2f
+#define VT_BOID_COHESION_WEIGHT             0.8f
+#define VT_BOID_SEPARATION_WEIGHT           1.0f
 
-// Precomputed radial force for banking
-static float g_boid_radial_force;
+// Steering force cap.
+#define VT_BOID_STEERING_CAP                10.0f
+
+// Acceleration blending factor: dt / (tau + dt), where tau is seconds to blend-in an acceleration update.
+#define VT_BOID_BLEND_FACTOR                (((float)VT_HZ / 1000) / (0.2 + ((float)VT_HZ / 1000)))
 
 // Trail history buffer size.
 #define VT_BOID_TRAIL_SIZE                  20
@@ -72,16 +81,17 @@ struct vec2f
 
 struct vt_boid
 {
-    // position, velocity and normal vectors, normals are unit vectors.
     struct vec2f p;
     struct vec2f v;
-    struct vec2f n;
 
-    // heading angles, in radians
-    float heading;
-    float desired_heading;
+    // heading angle, in radians
+    float h;
+
+    // Current angular speed
+    float w;
 
     // wandering state
+    float wander_angle;
     int heading_change_delay;
     int cur_heading_time;
 
@@ -231,24 +241,32 @@ static void debug_print(uint16_t row, uint16_t col, const char* fmt, ...)
     va_end(args);
 }
 
-// Implements small random changes to boid's heading after keeping the current heading for some time.
-static void wander(struct vt_boid* b, uint32_t dtime)
-{
-    b->cur_heading_time += dtime;
-
-    if (b->cur_heading_time >= b->heading_change_delay) {
-        b->cur_heading_time = 0;
-        b->heading_change_delay = random_value_spread(VT_BOID_AVG_HEADING_DELAY_MS, VT_BOID_HEADING_DELAY_VARIATION_MS);
-        b->desired_heading = b->heading + grad2rad(random_value_spread(0, VT_BOID_HEADING_CHANGE_LIMIT_DEG));
-    }
-}
-
-static void draw_debug_vec(struct vec2f origin, struct vec2f vec, int len, enum vtr_color fgc)
+static void debug_vec(struct vec2f origin, struct vec2f vec, float scale, enum vtr_color fgc)
 {
     struct vtr_vertex start = vec2f_project(origin);
-    struct vtr_vertex end = vec2f_project(vec2f_mul_add(origin, vec, len));
+    struct vtr_vertex end = vec2f_project(vec2f_mul_add(origin, vec, scale));
 
     vtr_scan_linec(g_vt, start.x, start.y, end.x, end.y, fgc);
+}
+
+// Apply a steering force (acceleration) vector to a boid to update its speed, position and heading.
+static void steer(struct vt_boid* b, struct vec2f fsteer, uint32_t dtime)
+{
+    // Record current position for trails before we change it.
+    b->trail[b->trail_idx] = b->p;
+    b->trail_idx = (b->trail_idx + 1) % VT_BOID_TRAIL_SIZE;
+    b->trail_len = MIN(b->trail_len + 1, VT_BOID_TRAIL_SIZE);
+
+    // Boid linear speed is fixed so we are only interested in the lateral component of the steering force.
+    // Compute that as lateral acceleration, get the angular speed from it and blend it into an accumulator.
+    // Then integrate blended angular speed into heading and position changes over dt.
+    float dts = (float)dtime / 1000;
+    float alat = vec2f_dot(vec2f_clamp(fsteer, VT_BOID_STEERING_CAP), vec2f_make(-sinf(b->h), cos(b->h)));
+    b->w = (1 - VT_BOID_BLEND_FACTOR) * b->w + VT_BOID_BLEND_FACTOR * alat;
+    b->h = fmodf(b->h + b->w * dts, 2.0f * M_PI);
+    b->p.x += VT_BOID_SPEED * cosf(b->h) * dts;
+    b->p.y += VT_BOID_SPEED * sinf(b->h) * dts;
+    b->v = heading_vec(b->h);
 }
 
 // Update simulation, dtime is in millisecs.
@@ -269,7 +287,7 @@ static void update(uint32_t dtime)
 
         size_t total_neighbors = 0;
         struct vec2f alignment = {0, 0};
-        struct vec2f cohesion = {0, 0};
+        struct vec2f centroid = {0, 0};
         struct vec2f separation = {0, 0};
         for (size_t j = 0; j < g_nboids; j++) {
             if (j == i) {
@@ -281,50 +299,76 @@ static void update(uint32_t dtime)
             if (dist_squared <= VT_BOID_VIEW_RANGE_SQUARED) {
                 total_neighbors += 1;
                 alignment = vec2f_add(alignment, other->v);
-                cohesion = vec2f_add(cohesion, other->p);
+                centroid = vec2f_add(centroid, other->p);
 
                 if (dist_squared <= VT_BOID_REPULSION_RANGE_SQUARED)
                 {
-                    // If distance rounds to 0 use a tiny distance instead to compute a repulsion vector.
+                    // Compute a repulsion vector to be stronger the closer this neighbor is to us.
+                    // Add an epsilon value to avoid potential div by 0.
                     struct vec2f repulsion = vec2f_mul(vec2f_sub(b->p, other->p),
-                                                       1.0f / (dist_squared == 0 ? 0.001f : dist_squared));
+                                                       (float)VT_BOID_REPULSION_RANGE / (dist_squared + FLT_EPSILON));
                     separation = vec2f_add(separation, repulsion);
                 }
             }
         }
 
         if (total_neighbors == 0) {
-            wander(b, dtime);
+            // Small random changes to boid's heading after keeping the current heading for some time.
+            b->cur_heading_time += dtime;
+            if (b->cur_heading_time >= b->heading_change_delay) {
+                b->cur_heading_time = 0;
+                b->heading_change_delay = random_value_spread(VT_BOID_AVG_HEADING_DELAY_MS, VT_BOID_HEADING_DELAY_VARIATION_MS);
+                b->wander_angle = grad2rad(random_value_spread(b->h, VT_BOID_HEADING_CHANGE_LIMIT_DEG));
+            }
+
+            struct vec2f fwander = heading_vec(b->wander_angle);
+            steer(b, fwander, dtime);
+
+            if (g_opt_debug) {
+                debug_print(i + 1, 0, "h = %+.02f, w = %+.02f, fwander = %.02f", b->h, b->w, vec2f_length(fwander));
+            }
         } else {
-            alignment = vec2f_unit(alignment);
+            // Reset wander state since we are in a flock now.
+            b->cur_heading_time = 0;
+            b->heading_change_delay = 0;
+            b->wander_angle = 0;
 
-            cohesion = vec2f_add(cohesion, b->p);
-            cohesion = (struct vec2f){cohesion.x / (total_neighbors + 1), cohesion.y / (total_neighbors + 1)};
-            cohesion = vec2f_unit(vec2f_sub(cohesion, b->p));
+            // Alignment vec points towards average direction of our neighbors while its magnitude
+            // is proportional to how much consensus they have with that direction.
+            // We'd like to be pulled stronger towards a shared alignment the more consensus it has.
+            alignment = vec2f_mul(alignment, VT_BOID_ALIGNMENT_WEIGHT / total_neighbors);
+            alignment = vec2f_clamp(alignment, VT_BOID_STEERING_CAP);
 
-            separation = vec2f_unit(separation);
+            // Cohesion vec points to the center-mass point of local flock.
+            // Convert that to a pull towards that centroid which is proportional to distance to the centroid.
+            // Maximum possible cohesion magnitude will approach the view distance, so we normalize by that.
+            centroid = vec2f_mul(vec2f_add(centroid, b->p), 1.0f / (total_neighbors + 1));
+            struct vec2f cohesion = vec2f_sub(centroid, b->p);
+            cohesion = vec2f_mul(cohesion, VT_BOID_COHESION_WEIGHT / VT_BOID_VIEW_RANGE);
+            cohesion = vec2f_clamp(cohesion, VT_BOID_STEERING_CAP);
 
-            // TODO: weighted adds
-            struct vec2f heading_vec = {0, 0};
-            heading_vec = vec2f_add(heading_vec, alignment);
-            heading_vec = vec2f_add(heading_vec, cohesion);
-            heading_vec = vec2f_add(heading_vec, separation);
+            // Separation vec points away from neighbors in repulsion range.
+            // The magnitude is iversely proportional to how close the neighbors are (e.g. how urgent it is).
+            separation = vec2f_mul(separation, VT_BOID_SEPARATION_WEIGHT);
+            separation = vec2f_clamp(separation, VT_BOID_STEERING_CAP);
 
-            b->desired_heading = heading_angle(heading_vec);
+            struct vec2f fsteer = {0, 0};
+            fsteer = vec2f_add(fsteer, alignment);
+            fsteer = vec2f_add(fsteer, cohesion);
+            fsteer = vec2f_add(fsteer, separation);
+
+            steer(b, fsteer, dtime);
+
+            if (g_opt_debug) {
+                debug_print(i + 1, 0, "h = %+.02f, w = %+.02f, falign = %.02f, fcoh = %.02f, fsep = %.02f",
+                            b->h, b->w, vec2f_length(alignment), vec2f_length(cohesion), vec2f_length(separation));
+                debug_vec(b->p, alignment, 10, VTR_COLOR_BLUE);
+                debug_vec(b->p, cohesion, 10, VTR_COLOR_GREEN);
+                debug_vec(b->p, separation, 10, VTR_COLOR_RED);
+                debug_vec(b->p, fsteer, 10, VTR_COLOR_DEFAULT);
+            }
         }
 
-        // Update heading to be closer to the desired heading over dt.
-        float dheading = g_boid_radial_force * dtime / 1000;
-        if (fabsf(b->heading - b->desired_heading) <= dheading) {
-            b->heading = b->desired_heading;
-        } else {
-            b->heading += (b->desired_heading > b->heading ? dheading : -dheading);
-        }
-
-        b->p.x += VT_BOID_SPEED * cosf(b->heading) * dtime / 1000;
-        b->p.y += VT_BOID_SPEED * sinf(b->heading) * dtime / 1000;
-        b->v = heading_vec(b->heading);
-        b->n = vec2f_normal(b->v);
 
         // Wrap over screen edges
         if (b->p.x < 0) {
@@ -345,11 +389,13 @@ static void draw(void)
 {
     for (size_t i = 0; i < g_nboids; i++) {
         struct vt_boid* b = g_boids + i;
+        struct vec2f d = vec2f_unit(b->v);
+        struct vec2f n = vec2f_normal(d);
 
         struct vtr_vertex buf[] = {
-            vec2f_project(vec2f_mul_add(b->p, b->n, -VT_BOID_WIDTH / 2)),
-            vec2f_project(vec2f_mul_add(b->p, b->n, VT_BOID_WIDTH / 2)),
-            vec2f_project(vec2f_mul_add(b->p, b->v, VT_BOID_LENGTH)),
+            vec2f_project(vec2f_mul_add(b->p, n, -VT_BOID_WIDTH / 2)),
+            vec2f_project(vec2f_mul_add(b->p, n, VT_BOID_WIDTH / 2)),
+            vec2f_project(vec2f_mul_add(b->p, d, VT_BOID_LENGTH)),
         };
 
         vtr_trace_polyc(g_vt, sizeof(buf) / sizeof(*buf), buf, b->color);
@@ -462,15 +508,11 @@ int main(int argc, char** argv)
 
     for (size_t i = 0; i < g_nboids; i++) {
         struct vt_boid* b = g_boids + i;
-        b->p = (struct vec2f){random_value_in_range(0, vtr_xdots(g_vt) - 1), random_value_in_range(0, vtr_ydots(g_vt) - 1)};
-        b->desired_heading = b->heading = grad2rad(random_value_in_range(0, 360));
-        b->v = heading_vec(b->heading);
-        b->n = vec2f_normal(b->v);
+        b->p = vec2f_make(random_value_in_range(0, vtr_xdots(g_vt) - 1), random_value_in_range(0, vtr_ydots(g_vt) - 1));
+        b->h = grad2rad(random_value_in_range(0, 360));
+        b->v = heading_vec(b->h);
         b->color = (g_opt_colors ? colors[i % (sizeof(colors) / sizeof(*colors))] : VTR_COLOR_DEFAULT);
     }
-
-    // The math below precomputes approximated boid radial force generated by a fixed banking angle
-    g_boid_radial_force = 9.81 * tanf(grad2rad(VT_BOID_BANK_ANGLE)) / VT_BOID_SPEED;
 
     uint64_t tcur, tprev = clock_monotonic_ms();
     uint32_t tdiff;
